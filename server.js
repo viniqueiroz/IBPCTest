bcfd50d1155b5527705c4f435f7b61d70fccd614
Vinicius Moura <viniciusmq@outlook.com>
2017-08-15 13:04:30 -0300

    fix server heroku
    minnor bug fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
   var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+  app = express();
+//  port = process.env.PORT || 3000;
 
 var mysqlModel = require('mysql-model');
 //var connection = require('./dbConnection');
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/materialRoutes');
 routes(app);
 
-app.listen(port, function() {
-    console.log("App is running on port " + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
