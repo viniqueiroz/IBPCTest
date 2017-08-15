90a6634fbbb85d4333567d12fe6de4ec0e51f023
Vinicius Moura <viniciusmq@outlook.com>
2017-08-15 12:42:04 -0300

    Configurar Get TAble
    teste de GET

--- api/routes/materialRoutes.js
+++ api/routes/materialRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   // todoList Routes
   app.route('/teste')
     .get(materialList.list_all_testes);
-    .post(materialList.add_teste);
+  //  .post(materialList.add_teste);
 
     app.use(function(req, res) {
       res.status(404).send({url: req.originalUrl + ' not found'})
