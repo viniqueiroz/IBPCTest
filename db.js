e9566377eacfd615511b03c1ae7895faec19b177
Vinicius Moura <viniciusmq@outlook.com>
2017-08-23 17:33:01 -0300

    Update db.js
    add heroku database informations

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 module.exports = {
     'connection': {
-     host     : 'us-cdbr-iron-east-05.cleardb.net',
-     user     : 'b57a5389ae7510',
-     password : 'c0d6620a',
-     database : 'heroku_214a997ce559eb2'
+     'host'     : 'us-cdbr-iron-east-05.cleardb.net',
+     'user'     : 'b57a5389ae7510',
+     'password' : 'c0d6620a'
     },
-	'database': 'ibpc',
+	'database': 'heroku_214a997ce559eb2',
     'users_table': 'usuario'
 };
