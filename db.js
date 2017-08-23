2e35d9c6441dbb95fe581d99a3af5074ff9c2190
Vinicius Moura <viniciusmq@outlook.com>
2017-08-23 17:18:41 -0300

    minnor bug fixes
    bugs

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 module.exports = {
     'connection': {
-        host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'b57a5389ae7510',
-  password : 'c0d6620a',
-database : 'heroku_214a997ce559eb2', 
+     host     : 'us-cdbr-iron-east-05.cleardb.net',
+     user     : 'b57a5389ae7510',
+     password : 'c0d6620a',
+     database : 'heroku_214a997ce559eb2'
     },
 	'database': 'ibpc',
     'users_table': 'usuario'
